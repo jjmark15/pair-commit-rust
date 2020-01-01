f706c25d69b7948c4d72d57c7567fd7318c07809
Josh Jones <ohblonddev@gmail.com>
2020-01-01 21:29:31 +0000

    add active flag and constructor with required fields

--- src/models.rs
+++ src/models.rs
@@ -2,8 +2,32 @@ pub mod author {
     pub struct Author<'a> {
         pub name: &'a str,
         pub email: &'a str,
+        active: bool,
     }
 
+    impl Default for Author<'_> {
+        fn default() -> Self {
+            Author {
+                name: "",
+                email: "",
+                active: false,
+            }
+        }
+    }
+
+    impl Author<'_> {
+        pub fn is_active(&self) -> bool {
+            return self.active;
+        }
+
+        pub fn new<'a>(name: &'a str, email: &'a str) -> Author<'a> {
+            return Author {
+                name,
+                email,
+                ..Author::default()
+            };
+        }
+    }
 
     #[cfg(test)]
     mod tests {
@@ -11,7 +35,7 @@ pub mod author {
 
         #[test]
         fn test_author_initialisation() {
-            let _author: Author = Author { name: "Tester", email: "test@test.com" };
+            let _author: Author = Author::new("Tester", "tester@test.com");
         }
     }
 }
