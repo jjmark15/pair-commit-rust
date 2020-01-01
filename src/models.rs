1140de132b0072903a38815dd5159f2aa1fe3724
Josh Jones <ohblonddev@gmail.com>
2020-01-01 20:28:10 +0000

    add fields to Author

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,17 @@
 pub mod author {
-    pub struct Author {}
-}
+    pub struct Author<'a> {
+        pub name: &'a str,
+        pub email: &'a str,
+    }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn test_author() {
-        assert_eq!(true, 1 == 1);
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_author_initialisation() {
+            let _author: Author = Author { name: "Tester", email: "test@test.com" };
+        }
     }
 }
