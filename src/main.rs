c373dde5261582a629fd0bb90745222ecff73865
Josh Jones <ohblonddev@gmail.com>
2020-01-02 12:44:40 +0000

    better error catching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use pair_commit_tool::models::author::{Author, AuthorVec};
+use pair_commit_tool::persistence::save;
+
 fn main() {
     println!("Hello, world!");
+    let mut authors = AuthorVec::new();
+    let author = Author::default();
+    authors.push(author);
+    save(authors);
 }
--- src/persistence.rs
+++ src/persistence.rs
@@ -1,20 +1,22 @@
 use std::fs::File;
-use std::io;
 use std::io::{ErrorKind, Write};
 
 use crate::models::author::AuthorVec;
 
-pub fn save(authors: AuthorVec) -> io::Result<()> {
+pub fn save(authors: AuthorVec) {
     let mut file = match File::create("temp/data.yml") {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => panic!("Problem creating the file: {:?}", error),
-            _ => panic!("Problem creating the file: {:?}", error),
+            _ => panic!("Unexpected problem creating the file: {:?}", error),
         }
     };
 
     match serde_yaml::to_writer(&file, &authors) {
-        Ok(()) => file.flush(),
-        Err(e) => panic!("Problem opening the file: {:?}", e)
+        Ok(()) => match file.flush() {
+            Ok(s) => s,
+            Err(error) => panic!("Problem writing data to file: {:?}", error)
+        },
+        Err(e) => panic!("Problem serializing authors to writer: {:?}", e)
     }
 }
