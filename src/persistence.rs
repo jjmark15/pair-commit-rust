90cd62c39ef90dd574bc4931de27d28eaf1c54a4
Josh Jones <ohblonddev@gmail.com>
2020-01-02 12:57:10 +0000

    test save

--- src/persistence.rs
+++ src/persistence.rs
@@ -3,8 +3,8 @@ use std::io::{ErrorKind, Write};
 
 use crate::models::author::AuthorVec;
 
-pub fn save(authors: AuthorVec) {
-    let mut file = match File::create("temp/data.yml") {
+pub fn save(file_path: &String, authors: AuthorVec) {
+    let mut file = match File::create(file_path) {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => panic!("Problem creating the file: {:?}", error),
@@ -18,5 +18,30 @@ pub fn save(authors: AuthorVec) {
             Err(error) => panic!("Problem writing data to file: {:?}", error)
         },
         Err(e) => panic!("Problem serializing authors to writer: {:?}", e)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::models::author::{Author, AuthorVec};
+    use crate::persistence::save;
+
+    #[test]
+    fn test_write_successful() {
+        let valid_path = "temp/data.yml".to_string();
+        let mut authors = AuthorVec::new();
+        let author = Author::default();
+        authors.push(author);
+        save(&valid_path, authors)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_write_unsuccessful() {
+        let path = "temps/data.yml".to_string();
+        let mut authors = AuthorVec::new();
+        let author = Author::default();
+        authors.push(author);
+        save(&path, authors);
     }
 }
