9ef1802d5d76178f74eeea2fb2f6e598a6a03618
Josh Jones <ohblonddev@gmail.com>
2020-01-02 17:16:03 +0000

    use `PathBuf` instead of strings

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,9 +1,10 @@
 use std::fs::File;
 use std::io::{ErrorKind, Write};
+use std::path::PathBuf;
 
 use pair_commit_tool::models::author::AuthorVec;
 
-pub fn save(file_path: &String, authors: AuthorVec) {
+pub fn save(file_path: PathBuf, authors: AuthorVec) {
     let mut file = match File::create(file_path) {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -21,7 +22,7 @@ pub fn save(file_path: &String, authors: AuthorVec) {
     }
 }
 
-pub fn load(file_path: &String) -> Result<AuthorVec, serde_yaml::Error> {
+pub fn load(file_path: PathBuf) -> Result<AuthorVec, serde_yaml::Error> {
     let file = File::open(file_path);
 
     match file {
@@ -34,6 +35,8 @@ pub fn load(file_path: &String) -> Result<AuthorVec, serde_yaml::Error> {
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use pair_commit_tool::models::author::{Author, AuthorVec};
 
     use crate::persistence::{load, save};
@@ -46,46 +49,47 @@ mod tests {
     }
 
     impl PersistenceFilePath {
-        pub fn get_filepath(&self) -> &str {
-            match self {
+        pub fn get_filepath(&self) -> PathBuf {
+            let string = match self {
                 PersistenceFilePath::Basic => "test_data/persistence/basic.yml",
                 PersistenceFilePath::Missing => "test_data/persistence/missing.yml",
                 PersistenceFilePath::Writable => "test_data/persistence/writable.yml",
                 PersistenceFilePath::MissingParent => "test_data/missing/missing.yml"
-            }
+            };
+            PathBuf::from(string)
         }
     }
 
     #[test]
     fn test_write_writable() {
-        let valid_path = PersistenceFilePath::Writable.get_filepath().to_string();
+        let path = PersistenceFilePath::Writable.get_filepath();
         let mut authors = AuthorVec::new();
         let author = Author::default();
         authors.push(author);
-        save(&valid_path, authors)
+        save(path, authors)
     }
 
     #[test]
     #[should_panic]
     fn test_write_missing_parent() {
-        let path = PersistenceFilePath::MissingParent.get_filepath().to_string();
+        let path = PersistenceFilePath::MissingParent.get_filepath();
         let mut authors = AuthorVec::new();
         let author = Author::default();
         authors.push(author);
-        save(&path, authors);
+        save(path, authors);
     }
 
     #[test]
     fn test_load_missing() {
-        let path = PersistenceFilePath::Missing.get_filepath().to_string();
-        let data = load(&path);
+        let path = PersistenceFilePath::Missing.get_filepath();
+        let data = load(path);
         assert_eq!(true, data.is_ok());
     }
 
     #[test]
     fn test_load_existing() {
-        let path = PersistenceFilePath::Basic.get_filepath().to_string();
-        let data = load(&path);
+        let path = PersistenceFilePath::Basic.get_filepath();
+        let data = load(path);
         assert_eq!(true, data.is_ok());
         let authors = data.unwrap();
         assert_eq!(false, authors.is_empty())
